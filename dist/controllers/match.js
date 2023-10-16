4a24350296eb3cb80ab22afa91bc7cf2808a9d84
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-10-16 11:25:09 -0400

    ignore dist folder

--- dist/controllers/match.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// import express, { type NextFunction, type Request, type Response } from 'express'
-// export const listMatches = (req: Request, res: Response, next: NextFunction) => {
-// todo sth
-// }
