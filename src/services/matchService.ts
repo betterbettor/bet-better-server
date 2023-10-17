d9cfa6a222976411c1263f3d13fe5b01fdbae3c1
charrrleee <charlcpy@gmail.com>
2023-10-17 00:26:18 -0400

    create match and odd service to connect db table

---
+++ src/services/matchService.ts
@@ -0,0 +1,7 @@
+import Match from '../interfaces/match.interface';
+import MatchSchema from '../models/matchModel';
+
+const getMatchList = async () : Promise<Match[]> => {
+  return await MatchSchema.find({}).lean();
+}
+export default { getMatchList };
