15b4ffd6773a9616d632b4a238607ec7f62e99d0
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-10-23 12:01:59 -0400

    created indexes on ids and ttl

--- src/services/oddsService.ts
+++ src/services/oddsService.ts
@@ -2,7 +2,7 @@ import Odds from '../interfaces/odds.interface';
 import OddSchema from '../models/oddsModel';
 
 const getOddsByMatchId = async (matchId: number): Promise<Odds[]> => {
-  return await OddSchema.find({ matchId }).sort({ timestamp: 'asc' }).exec();
+  return await OddSchema.find({ matchId }, '-ttl', { lean: true }).sort({ timestamp: 'asc' }).exec();
 };
 
 const createOdds = async (odds: Odds[]): Promise<boolean> => {
